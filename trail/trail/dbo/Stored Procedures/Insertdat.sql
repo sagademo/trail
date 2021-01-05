da0d177dc5ed64ecce3e25e11185e562e59170f2
admin <admin@MOUNTAIN>
2021-01-05 17:11:17 +0530

    insertdata

---
+++ trail/trail/dbo/Stored Procedures/Insertdat.sql
@@ -0,0 +1,9 @@
+﻿create procedure Insertdat 
+(@name VARCHAR(10),@salary INT,@StatementType nvarchar(20) = '' )  
+AS  
+BEGIN  
+IF @StatementType = 'Insert'  
+BEGIN  
+insert into employee(name,salary) values( @name,@salary)  
+END  
+end
